8c75aa91aa7e95c3a71f86346690ccb1306c901a
Honey-Joe <honeyjoe943@gmail.com>
2024-07-20 23:09:07 +0530

    update contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     const schema = z.object({
         name: z.string().min(1, { message: 'Required' }),
         email: z.string().min().email(),
-        phn_no: z.string().min(10),
+        phn_no: z.string().min(10 ,{message:"please enter correct phone number"}),
         messagetext:z.string().min()
       });
   
